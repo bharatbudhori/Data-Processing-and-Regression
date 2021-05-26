a2c233cfcaa6749d002a9548c794444e21c749f6
bharatbudhori <70325600+bharatbudhori@users.noreply.github.com>
2021-05-26 18:02:27 +0530

    Alter dialogs, snackbar, bottomsheet

--- lib/main.dart
+++ lib/main.dart
@@ -1,61 +1,145 @@
 import 'package:flutter/material.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
-//import 'package:location/location.dart';
+import 'package:get/get.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Great Places',
-      theme: ThemeData(
-        primarySwatch: Colors.indigo,
-        accentColor: Colors.amber,
-      ),
-      home: GoogleMapPage(),
+      title: 'Showing Alret SnackBar',
+      home: DemoApp(),
     );
   }
 }
 
-class GoogleMapPage extends StatefulWidget {
-  @override
-  _GoogleMapPageState createState() => _GoogleMapPageState();
-}
-
-class _GoogleMapPageState extends State<GoogleMapPage> {
-  // Future<void> _getCurrentUserLocation() async {
-  //   final locData = await Location().getLocation();
-
-  //   LatLng myTarget = LatLng(locData.latitude, locData.longitude);
-  //   return myTarget;
-  // }
-
+class DemoApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Google Map Live Tracking'),
+        title: Text('SnackBar'),
       ),
-      body: Column(
-        children: [
-          Expanded(
-              child: GoogleMap(
-            mapType: MapType.normal,
-            initialCameraPosition: CameraPosition(
-              target: LatLng(28.7041, 77.1025),
-              zoom: 18,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            ElevatedButton.icon(
+              onPressed: () {
+                showSnackBar();
+              },
+              icon: Icon(Icons.add_alert),
+              label: Text('Show SnackBar'),
             ),
-            onMapCreated: (GoogleMapController controller) {},
-          ))
-        ],
+            ElevatedButton.icon(
+              onPressed: () {
+                showDialog();
+              },
+              icon: Icon(Icons.settings_display_outlined),
+              label: Text('Show Dialog'),
+            ),
+            ElevatedButton.icon(
+              onPressed: () {
+                showBottomsheet();
+              },
+              icon: Icon(Icons.settings_display_outlined),
+              label: Text('Show Bottom Sheet'),
+            ),
+          ],
+        ),
       ),
     );
   }
 }
 
-class PlaceLocation {
-  double latitude;
-  double longitude;
+void showSnackBar() {
+  Get.snackbar(
+    'GetX SnackBar', 'This snackbar requires no context',
+    duration: Duration(milliseconds: 1500),
+    //snackPosition: SnackPosition.BOTTOM,
+    colorText: Colors.deepOrange,
+    backgroundColor: Colors.black,
+    backgroundGradient: LinearGradient(
+      colors: [Colors.amberAccent, Colors.tealAccent],
+      begin: Alignment.topLeft,
+      end: Alignment.bottomRight,
+    ),
+    dismissDirection: SnackDismissDirection.HORIZONTAL,
+    overlayBlur: 2,
+    //showProgressIndicator: true,
+    //userInputForm: Form(child: TextField()),
+    onTap: (snack) {
+      print('Snackbar tapped');
+    },
+  );
+}
+
+void showDialog() {
+  Get.defaultDialog(
+    title: 'Alert Dialog',
+    middleText: "Do you want to close dialog?",
+    content: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        CircularProgressIndicator(),
+        Text('Loading'),
+      ],
+    ),
+    actions: [
+      TextButton(
+        onPressed: () {
+          Get.back();
+        },
+        child: Text('Ok'),
+      ),
+    ],
+  );
+}
+
+void showBottomsheet() {
+  Get.bottomSheet(
+    Container(
+      color: Colors.white,
+      height: 160,
+      child: Wrap(
+        children: [
+          ListTile(
+            leading: Icon(
+              Icons.nights_stay_rounded,
+              color: Colors.black,
+            ),
+            title: Text(
+              'Dark Theem',
+              style: TextStyle(color: Colors.black),
+            ),
+            onTap: () {
+              Get.changeTheme(
+                ThemeData.dark(),
+              );
+            },
+          ),
+          Divider(),
+          ListTile(
+            leading: Icon(
+              Icons.wb_sunny,
+              color: Colors.yellow,
+            ),
+            title: Text(
+              'Light Theem',
+              style: TextStyle(color: Colors.black),
+            ),
+            onTap: () {
+              Get.changeTheme(
+                ThemeData.light(),
+              );
+            },
+          ),
+        ],
+      ),
+    ),
+    backgroundColor: Colors.black,
+    enableDrag: true,
+  );
 }
